31ad0f9ac064ea15227b25fe51438e194785851a
Anirud Samala <anirud.samala@gmail.com>
2017-02-07 00:57:58 +0530

    django: added upload+store functionality
    uploads to src/server/submissions

--- src/server/trial/forms.py
+++ src/server/trial/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 # Form Classes defined here, to be used to render forms directly into templated
 class SubmissionForm(forms.Form):
-    file = forms.FileField()
+    submission_file = forms.FileField()
--- src/server/trial/urls.py
+++ src/server/trial/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^$', views.upload, name='upload'),
+    url(r'^upload/', views.upload, name='upload'),
 ]
