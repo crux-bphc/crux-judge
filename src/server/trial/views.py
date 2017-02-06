3f76c34ceb77ebfb8c5dbd9edad40e814dc6faa4
Anirud Samala <anirud.samala@gmail.com>
2017-02-06 23:04:40 +0530

    django: Add 'trial' to INSTALLED_APPS, add /trial url for file upload.

--- src/server/trial/views.py
+++ src/server/trial/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import SubmissionForm
+
 # Create your views here.
+def index(request):
+    submission_form = SubmissionForm()
+    # contains all variables passed to template while rendering
+    context = {
+        "submission_form": submission_form,
+    }
+    # throws a html response with
+    return render(request,'index.html',context)
