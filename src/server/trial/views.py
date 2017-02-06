aa68f39948e174744e2849e24967ad0d3b127822
Anirud Samala <anirud.samala@gmail.com>
2017-02-06 23:59:29 +0530

    django: Add file submission form on /trial
    django: added file submission form - localhost:8000/trial
    form class - src/server/trial/forms.py
    request url - src/server/trial/urls.py
    /trial template - src/server/trial/templates/index.html
    
    django: added upload+store functionality
    uploads to src/server/submissions
    
    django: create submissions folder if not present
    
    gitignore: ignore src/server/submissions contents

--- src/server/trial/views.py
+++ src/server/trial/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from .forms import SubmissionForm
+import os
 
 # Create your views here.
 def index(request):
@@ -12,3 +13,21 @@ def index(request):
     }
     # throws a html response with
     return render(request,'index.html',context)
+
+def upload(request):
+    if request.method == "POST":
+        uploaded_filedata = request.FILES["submission_file"]
+        #creates src/server/submissions folder if does not exist
+        if not os.path.isdir("submissions"):
+            os.makedirs("submissions")
+        # creates new file in src/server/submissions
+        filepath = "submissions/"+request.FILES["submission_file"].name
+        submission_file = open(filepath,"wb+")
+        # write to file - failsafe for handling large file
+        for chunk in uploaded_filedata.chunks():
+            submission_file.write(chunk)
+        # insert submission file handler below
+        # compile("submissions/")
+        return HttpResponse("file uploaded successfully")
+    else:
+        return HttpResponse("trial/upload/")
