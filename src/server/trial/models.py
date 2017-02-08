fa9237cf6296fed70e132d57e995767cd0b6ecf8
Hitesh Bhagchandani <hiteshbhagchandani39@gmail.com>
2017-02-08 22:51:07 +0530

    Added model for 'Problems'. (yet to be verified)

--- src/server/trial/models.py
+++ src/server/trial/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
-# Create your models here.
+from django.utils.encoding import python_2_unicode_compatible
+from django.contrib.auth.models import User
+#Problems: id,title, statement, output, uploadedby
+#2. Admins: id, name, email, passwordhash
+#3. Students: id, name, email, passwordhash
+@python_2_unicode_compatible
+class Problem(models.Model):
+    problem_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50,unique=False)
+    statement = models.CharField(max_length=1000,unique=False)
+    output = models.CharField(max_length=100,unique=False)
+    uploadedby = models.ForeignKey('Admin',verbose_name="problem-setter")
+    class Meta:
+        verbose_name='problem'
+        ordering = ['id']
