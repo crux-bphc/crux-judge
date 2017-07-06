4290f37b0abf6a36c83fc015300629d7026aaa85
Basu Dubey <basu.96@gmail.com>
2017-07-06 11:27:38 +0530

    Shifted database to MySQL. Updated docs. Edited Models

--- src/server/trial/models.py
+++ src/server/trial/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.contrib.auth.models import User
-#Problems: id,title, statement, output, uploadedby
+
+#1. Problems: id,title, statement, output, uploadedby
 #2. Admins: id, name, email, passwordhash
 #3. Students: id, name, email, passwordhash
 @python_2_unicode_compatible
@@ -10,7 +11,7 @@ class Problem(models.Model):
     title = models.CharField(max_length=50,unique=False)
     statement = models.CharField(max_length=1000,unique=False)
     output = models.CharField(max_length=100,unique=False)
-    uploadedby = models.ForeignKey('Admin',verbose_name="problem-setter")
+    uploadedby = models.ForeignKey(User,verbose_name="problem-setter")
     class Meta:
         verbose_name='problem'
-        ordering = ['id']
+        ordering = ['problem_id']
