8a880cb7fa037ebc590e8c594152db46562dc9c7
Basu Dubey <basu.96@gmail.com>
2017-07-14 23:57:40 +0530

    Contest Login

--- src/server/judge/urls.py
+++ src/server/judge/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^trial/', include('trial.urls')),
+    url(r'^contest/', include('contest.urls'))   
 ]
