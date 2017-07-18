8bbcfc5aedeac9840cdbad5ab6e381027a2971b7
Basu Dubey <basu.96@gmail.com>
2017-07-18 13:17:23 +0530

    Contest web site prototype

--- src/server/contest/urls.py
+++ src/server/contest/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login', views.login, name='login')
+    url(r'^login', views.login, name='login'),
+    url(r'^problem/(?P<problem_id>[0-9]+)/$', views.problem, name='problem'),
 ]
