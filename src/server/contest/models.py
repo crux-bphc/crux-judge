3618fa13081a1ad6c228d1324ee6eb7aea405397
Basu Dubey <basu.96@gmail.com>
2017-07-18 13:17:23 +0530

    Contest web site prototype

--- src/server/contest/models.py
+++ src/server/contest/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.utils.encoding import python_2_unicode_compatible
+# from django.contrib.auth.models import User
 # Create your models here.
+
+@python_2_unicode_compatible
+
+class Problem(models.Model):
+    problem = models.ForeignKey('trial.problem',verbose_name='problem')
+    class Meta:
+        verbose_name = 'Contest Problem'
