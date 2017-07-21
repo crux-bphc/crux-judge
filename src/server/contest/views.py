d35010fc38c69251b3a318f19a6780942ee7f60f
Basu Dubey <basu.96@gmail.com>
2017-07-21 22:10:59 +0530

    contest backend improvements

--- src/server/contest/views.py
+++ src/server/contest/views.py
@@ -1,12 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import LoginForm
+from .forms import LoginForm,SubmissionForm
 from .models import Problem as contest_problem
-from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 from trial.models import Problem as all_problems
+from time import sleep
+import os
+from datetime import datetime
 
 # Create your views here.
 def index(request):
+    # request.session.clear_expired()
+
+    if request.user.is_authenticated():
+        return problemList(request)
+
     login_form = LoginForm()
 
     context = {
@@ -16,7 +25,7 @@ def index(request):
     return render(request,'contest_login.html',context)
 
 
-def login(request):
+def auth(request):
     if request.method == "POST":
 
         form = LoginForm(request.POST)
@@ -24,38 +33,76 @@ def login(request):
             username = form["username"].value()
             password = form["password"].value()
 
-        user = authenticate(username=username,password=password)
+        user = authenticate(request,username=username,password=password)
 
         if user is not None and user.is_active:
+            # login_ successful
+            login(request,user)
+            # user_id = request.session['_auth_user_id']
+            # username = User.objects.get(id = user_id).username
+            request.session.set_expiry(0) #session expires when browser is closed
 
-            request.session.set_expiry(300) #session expires within 5 minutes of logging in
-            titles = []
-            ids = []
-
-            problems = contest_problem.objects.all()
-            for problem in problems:
-                titles.append(problem.problem.title)
-                ids.append(problem.problem_id)
-
-            context = {'data':list(zip(ids,titles))}
-            return render(request,"problem_page.html",context)
+            return problemList(request)
 
         else:
+            # login_ failed
             context = {"login_form":LoginForm()}
             return render(request,'contest_login.html',context)
     else:
-        return HttpResponse("contest/login/")
+        return HttpResponse("contest/auth/")
 
 def problem(request,problem_id):
-    if 'sessionid' in request.COOKIES:
+
+    if request.user.is_authenticated:
         problem = all_problems.objects.get(problem_id=problem_id)
-        return render(request,'problem.html',{'problem':problem})
+        user = User.objects.get(id=request.session['_auth_user_id'])
+        submission_form = SubmissionForm()
+        context = {
+            "submission_form" : submission_form,
+            "problem" : problem,
+            "username" : user.username
+        }
+        return render(request,'problem.html',context)
     else:
         return HttpResponse("Session Expired. Login again")
 
-def logout(request):
-    try:
-        del request.COOKIES['sessionid']
-    except KeyError:
-        pass
+def problemList(request):
+
+    titles = []
+    ids = []
+    problems = contest_problem.objects.all()
+    user = User.objects.get(id=request.session['_auth_user_id'])
+
+    for problem in problems:
+        titles.append(problem.problem.title)
+        ids.append(problem.problem_id)
+
+    context = {
+        'data':list(zip(ids,titles)),
+        'username':user.username
+    }
+    return render(request,'problem_page.html',context)
+
+
+def upload(request):
+
+    if request.method == "POST":
+        uploaded_filedata = request.FILES["submission_file"]
+        #creates /contest/submissions folder if does not exist
+        if not os.path.isdir("submissions"):
+            os.makedirs("submissions")
+        # creates new file in /contest/submissions
+        filepath = "submissions/"+request.FILES["submission_file"].name
+        submission_file = open(filepath,"wb+")
+        # write to file - failsafe for handling large file
+        for chunk in uploaded_filedata.chunks():
+            submission_file.write(chunk)
+        # insert submission file handler below
+        # compile("submissions/")
+        return HttpResponse("File uploaded successfully")
+    else:
+        return HttpResponse("/contest/upload/")
+
+def logout_view(request):
+    logout(request)
     return HttpResponse("You're logged out.")
