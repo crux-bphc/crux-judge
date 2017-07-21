91d617c7b1e0b790a37283da98939f0dc7f37793
Basu Dubey <basu.96@gmail.com>
2017-07-21 22:10:59 +0530

    contest backend improvements

--- src/server/contest/forms.py
+++ src/server/contest/forms.py
@@ -3,3 +3,6 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(label='ID',max_length=20)
     password = forms.CharField(widget=forms.PasswordInput)
+
+class SubmissionForm(forms.Form):
+    submission_file = forms.FileField()
