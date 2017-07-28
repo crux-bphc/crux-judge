13a1e9c3398eb197cb7b6ec7045f8fc3833ce796
Basu Dubey <basu.96@gmail.com>
2017-07-29 03:40:01 +0530

    submissions: Add UI to view all submissions.

--- src/server/contest/admin.py
+++ src/server/contest/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Problem
+from .models import Problem, Submission
 
 # Register your models here.
 admin.site.register(Problem)
+admin.site.register(Submission)
