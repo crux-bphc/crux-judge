5963d19df2320b8b8cf221a78e941db5e409d5d6
Basu Dubey <basu.96@gmail.com>
2018-04-07 04:39:55 +0530

    settings: Improve static files serving settings

--- src/server/judge/urls.py
+++ src/server/judge/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^', include('contest.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^bank/', include('bank.urls')),
     url(r'^contest/', include('contest.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
